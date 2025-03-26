a35e444fa617ebe430d85a06e28224efe64b231c
FARUK2059 <omarfaruk34552@gmail.com>
2025-03-26 13:09:46 +0600

    add foren key

---
+++ foreignKey.sql
@@ -0,0 +1,13 @@
+
+-- Create table
+CREATE TABLE "user"(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(25) NOT NULL
+);
+
+
+CREATE TABLE post(
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    user_id INTEGER REFERENCES "user"(id)
+)
